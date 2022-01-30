6129a46ef5bc208457944f592f35d43ab46ff65d
alexw-1 <thor.wiegand@gmail.com>
2022-01-29 20:57:28 -0500

    added nav stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import logo from './ourLogo.png'
-// import WebcamStream from './webcamjs';
-
-
-
+import { Outlet, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
@@ -12,16 +9,14 @@ function App() {
         <a href="#default" className="logo"><img src={logo} width="200" height="50" alt="" /></a>
           <span id='app_title'>DOBUTSU</span>
           <div className="header-right">
-            {/* <Router>
-              <Route path=""/>
-            </Router> */}
-            <a href="https://teachablemachine.withgoogle.com/models/ymybQkyPh/">Machine Learning</a>
-            <a className="active" href="#home">Home</a>
-            <a href="#contact">Contact</a>
-            <a href="#about">About</a>
+            <a target="_blank" rel="noreferrer" href="https://teachablemachine.withgoogle.com/models/ymybQkyPh/">Machine Learning</a>
+            <Link className="active" to="/">Home</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/about">About</Link>
           </div>
         </div>
       </header>
+      <Outlet />
     </div>
   );
 }
---
+++ src/homepage.js
@@ -0,0 +1,5 @@
+export default function Home(){
+    return (
+        <div><h1>HOMEPAGE PAGE</h1></div>
+    );
+}
