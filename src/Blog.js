48528f14f7936b31a54be01b99007dac137dda09
SySyAli <35876523+SySyAli@users.noreply.github.com>
2022-01-30 09:31:11 -0500

    blog

--- src/Blog.js
+++ src/Blog.js
@@ -1,18 +1,18 @@
 import './Blog.css';
 export default function Blog() {
     return (
-        <div class="leftcolumn">
-            <div class="card">
-                <h2>TITLE HEADING</h2>
+        <div className="leftcolumn">
+            <div className="card">
+                <h1>TITLE HEADING</h1>
                 <h5>Title description, Dec 7, 2017</h5>
-                <div class="fakeimg" style="height:200px;">Image</div>
+                <div className="fakeimg">Image</div>
                 <p>Some text..</p>
                 <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
             </div>
-            <div class="card">
+            <div className="card">
                 <h2>TITLE HEADING</h2>
                 <h5>Title description, Sep 2, 2017</h5>
-                <div class="fakeimg" style="height:200px;">Image</div>
+                <div className="fakeimg">Image</div>
                 <p>Some text..</p>
                 <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
             </div>
